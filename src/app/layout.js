92b6f57b400c7c22956845c039c235170d693e42
Ibra7emels3id <ibrahemelsaid692@gmail.com>
2024-06-21 00:27:36 +0300

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,19 +8,18 @@ import "../../node_modules/react-toastify/dist/ReactToastify.css";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { Analytics } from '@vercel/analytics/react';
-import AuthProvider from './Provider/AuthProvider'
-
+import AuthProvider from './provider/AuthProvider.js'
 
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body className={inter.className}>
-                <AuthProvider>
-                    <Provider store={store}>
+                <Provider store={store}>
+                    <AuthProvider>
                         {children}
-                    </Provider>
-                </AuthProvider>
+                    </AuthProvider>
+                </Provider>
             </body>
         </html>
     );
