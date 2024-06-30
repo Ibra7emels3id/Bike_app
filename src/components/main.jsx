57e0c5b042ba9bf19b20aeb261195b66e5638db6
Ibra7emels3id <ibrahemelsaid692@gmail.com>
2024-06-30 18:09:15 +0300

    first commit

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -3,8 +3,7 @@ import React from 'react';
 const Main = () => {
     return (
         <>
-            <section
-                className="relative bg-imgs bg-[url(https://surlybikes.com/uploads/bikes/_medium_image/Lowside_BK0534_Background-2000x1333.jpg)] "
+            <section className="relative bg-imgs bg-[url(https://surlybikes.com/uploads/bikes/_medium_image/Lowside_BK0534_Background-2000x1333.jpg)] "
             >
                 <div
                     className="absolute inset-0 bg-gray-900/75 sm:bg-transparent sm:from-gray-900/95 sm:to-gray-900/25 ltr:sm:bg-gradient-to-r rtl:sm:bg-gradient-to-l"
@@ -13,7 +12,7 @@ const Main = () => {
                 <div
                     className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
                 >
-                    <div className="main-text max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+                    <div className="main-text z-50 max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
                         <h1 className="text-3xl text-start font-extrabold text-white sm:text-5xl">
                             Let us find your
 
